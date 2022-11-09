ce241ca2b870b352ec02fb1d371ae853b03e356c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-11-09 13:30:13 -0500

    chore: Add the starting version of the run-sizeof.go program with TODO.

---
+++ project/golang/run-sizeof.go
@@ -0,0 +1,23 @@
+// TODO: Implement a Go program that accurately reports all of
+// the information requested in the README for this project
+//
+// TODO: Make sure that you do not hard-code this program's output!
+
+// TODO: Review these (and any other needed) references:
+//
+// https://www.callicoder.com/golang-basic-types-operators-type-conversion/
+// https://stackoverflow.com/questions/21511142/equivalent-of-sizeofatype-in-go
+// https://dlintw.github.io/gobyexample/public/memory-and-sizeof.html
+// https://pkg.go.dev/unsafe
+
+// TODO: Make sure that your program provides all of the following output
+// and takes all of the following steps
+
+// 1. display the operating system information
+// 2. display the computer architecture information
+// display the sizing information for the four data types
+// a. char
+// b. int
+// c. float
+// d. double
+// 3. make sure that the program returns the correct exit code
