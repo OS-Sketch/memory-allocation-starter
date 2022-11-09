b4a69b69588be8ce31b68cf7fec08a6c7e369e68
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2022-11-09 13:28:11 -0500

    chore: Add the starting version of run-sizeof.c for clang with TODO.

---
+++ project/clang/run-sizeof.c
@@ -0,0 +1,23 @@
+// TODO: Implement a C program that accurately reports all of
+// the information requested in the README for this project
+//
+// TODO: Make sure that you do not hard-code this program's output!
+
+// TODO: Review these (and any other needed) references:
+//
+// https://www.tutorialspoint.com/sizeof-operator-in-c
+// https://www.geeksforgeeks.org/sizeof-operator-c/
+// https://stackoverflow.com/questions/26416703/please-explain-the-output-of-sizeof
+// https://iq.opengenus.org/detect-operating-system-in-c/
+
+// TODO: Make sure that your program provides all of the following output
+// and takes all of the following steps
+
+// 1. display the operating system information
+// 2. display the computer architecture information
+// display the sizing information for the four data types
+// a. char
+// b. int
+// c. float
+// d. double
+// 3. make sure that the program returns the correct exit code
